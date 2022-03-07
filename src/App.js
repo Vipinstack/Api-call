ccbcf14fabf7b62bb594fee1d501868a479c2ece
VipinStack <Vipinchauhanyaski@example.com>
2022-03-07 13:53:54 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import './App.css';
 import Dropdown from './Dropdown';
 import Api from './Api';
 import {ToastContainer} from 'react-toastify';
+import Pratice from './Pratice';
 
 function App() {
   return (
     <div>
       <ToastContainer />
+      <Pratice />
+      <br /><br />
       <Dropdown />
       <Api />
     </div>
